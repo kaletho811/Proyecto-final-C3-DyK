a51dd3990e0e0740bc870024fffe8683a8e8ffda
Kaleth Barrios <kaletht@yahoo.com>
2025-05-19 16:44:42 -0500

    intento correccion xd

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import RegistroPagina from './paginas/RegistroPagina';
 import CarritoPagina from './paginas/CarritoPagina';
 import Header from './componentes/Header';
 
+
 function App() {
   return (
     <Router>
